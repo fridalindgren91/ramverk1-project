6956049379fa9877597b7e60170a1d81b75378fa
frlg18 <fridalindgren_@outlook.com>
2020-03-09 21:58:25 +0100

    changed header

--- config/navbar/header.php
+++ config/navbar/header.php
@@ -68,7 +68,7 @@ if (isset($_SESSION["username"])) {
             ],
             [
                 "text" => "Taggar",
-                "url" => "tags",
+                "url" => "question/tags",
                 "title" => "Taggar",
             ],
             [
